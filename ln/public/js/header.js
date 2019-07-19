1e7125c8b53f9159054389dfefde6b4934dfadbc
hxg <1026166072@qq.com>
2019-07-19 11:57:42 +0800

    0

---
+++ ln/public/js/header.js
@@ -0,0 +1,12 @@
+
+$(function(){
+  $.ajax({
+    url:"header.html",
+    type:"get",
+    success:function(result){
+      console.log(result);
+      $(result).replaceAll("#header");
+      $(`<link rel="stylesheet href="css/comm.css">`).appendTo("head");
+    }
+  })
+})
