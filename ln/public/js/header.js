76cd87ed022f0495f62dd9a887234c73ffa56f14
Zyh994 <1360347636@qq.com>
2019-08-17 15:37:06 +0800

    ..

--- ln/public/js/header.js
+++ ln/public/js/header.js
@@ -1,4 +1,3 @@
-
 $(function(){
   $.ajax({
     url:"header.html",
@@ -6,8 +5,19 @@ $(function(){
     success:function(result){
       $(result).replaceAll("#header");
       $(`<link rel="stylesheet href="css/comm.css">`).appendTo("head");
+      setTimeout(function(){
+        var h = $(".navbar").css("height");
+        // h = Number(h.split("px")[0])+20+"px";
+        $(".main").css("margin-top",h);
+      },50)
     }
-  })
+  });
+  setTimeout(function(){
+    $(".zyh").animate({
+      left:"1400px"
+    },1000)
+  },50)
 })
 
 
+  
